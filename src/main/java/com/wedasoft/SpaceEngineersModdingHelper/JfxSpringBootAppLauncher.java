b79a41226b63135b0aa1b5a74b432f68d2a2e238
David Weber <weberdavid91@gmail.com>
2025-01-10 15:57:03 +0100

    initial commit

---
+++ src/main/java/com/wedasoft/SpaceEngineersModdingHelper/JfxSpringBootAppLauncher.java
@@ -0,0 +1,131 @@
+package com.wedasoft.SpaceEngineersModdingHelper;
+
+import javafx.application.Application;
+import javafx.application.HostServices;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.*;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@SpringBootApplication
+@Slf4j
+public class JfxSpringBootAppLauncher {
+
+    public static final String LOG_PREFIX = "JavaFx Spring Boot lifecycle: ";
+    public static final String SPLASHSCREEN_CLASS_PATH = "com.wedasoft.SpaceEngineersModdingHelper.SplashScreenPreloader";
+
+    public static void main(String[] args) {
+        log.info(LOG_PREFIX + "Starting application via main method...");
+
+        log.info(LOG_PREFIX + "Set full qualified class name as system property for defining the splash screen preloader.");
+        System.setProperty("javafx.preloader", SPLASHSCREEN_CLASS_PATH);
+
+        log.info(LOG_PREFIX + "Launching the JavaFx application.");
+        Application.launch(JfxSpringBootApp.class, args);
+    }
+
+    public static class JfxSpringBootApp extends Application {
+
+        private static ConfigurableApplicationContext springApplicationContext;
+
+        @Override
+        public void init() {
+            log.info(LOG_PREFIX + "Executing overridden 'init()' of JavaFx Application...");
+            log.info(LOG_PREFIX + "Creating the spring application context.");
+            try {
+                springApplicationContext = new SpringApplicationBuilder()
+                        .sources(JfxSpringBootAppLauncher.class)
+                        .initializers((ApplicationContextInitializer<GenericApplicationContext>) applicationContext -> {
+                            applicationContext.registerBean(Application.class, () -> this);
+                            applicationContext.registerBean(Parameters.class, this::getParameters);
+                            applicationContext.registerBean(HostServices.class, this::getHostServices);
+                        })
+                        .run(getParameters().getRaw().toArray(new String[0]));
+            } catch (Throwable t) {
+                log.error(t.getMessage(), t);
+                throw new RuntimeException(t);
+            }
+        }
+
+        @Override
+        public void start(Stage primaryStage) {
+            log.info(LOG_PREFIX + "Executing overridden 'start()' of JavaFx Application...");
+            log.info(LOG_PREFIX + "Publishing event 'JfxApplicationStartEvent'.");
+            try {
+                springApplicationContext.publishEvent(new JfxApplicationStartEvent(primaryStage));
+            } catch (Throwable t) {
+                log.error(t.getMessage(), t);
+                throw new RuntimeException(t);
+            }
+        }
+
+        @Override
+        public void stop() {
+            log.info(LOG_PREFIX + "Executing overridden 'stop()' of JavaFx Application...");
+            springApplicationContext.close();
+            Platform.exit();
+            System.exit(0);
+        }
+
+    }
+
+    @Component
+    public static class JfxApplicationStartEventListener implements ApplicationListener<JfxApplicationStartEvent> {
+
+        @Value("${spring.application.name}")
+        private final String applicationTitle;
+        private final ApplicationContext springApplicationContext;
+
+        public JfxApplicationStartEventListener(
+                @Value("${spring.application.name}") String applicationTitle,
+                ApplicationContext springApplicationContext) {
+
+            this.applicationTitle = applicationTitle;
+            this.springApplicationContext = springApplicationContext;
+        }
+
+        @Override
+        public void onApplicationEvent(JfxApplicationStartEvent event) {
+            try {
+                log.info(LOG_PREFIX + "Computing 'JfxApplicationStartEvent'...");
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/wedasoft/SpaceEngineersModdingHelper/views/ui.fxml"));
+                fxmlLoader.setControllerFactory(springApplicationContext::getBean);
+                Parent root = fxmlLoader.load();
+                Scene scene = new Scene(root, 600, 600);
+                Stage stage = event.getStage();
+                stage.setScene(scene);
+                stage.setTitle(this.applicationTitle);
+                stage.show();
+                log.info(LOG_PREFIX + "JavaFx Spring boot application started.");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            if (SplashScreenPreloader.stage != null) {
+                log.info(LOG_PREFIX + "Closing splash screen.");
+                SplashScreenPreloader.stage.close();
+            }
+        }
+
+    }
+
+    public static class JfxApplicationStartEvent extends ApplicationEvent {
+        public Stage getStage() {
+            return (Stage) getSource();
+        }
+
+        public JfxApplicationStartEvent(Stage source) {
+            super(source);
+        }
+    }
+
+}
---
+++ src/main/java/com/wedasoft/SpaceEngineersModdingHelper/views/UiController.java
@@ -0,0 +1,69 @@
+package com.wedasoft.SpaceEngineersModdingHelper.views;
+
+import com.wedasoft.SpaceEngineersModdingHelper.persistence.todo.Todo;
+import com.wedasoft.SpaceEngineersModdingHelper.persistence.todo.TodoRepository;
+import com.wedasoft.SpaceEngineersModdingHelper.services.JfxUiService;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Dimension2D;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@RequiredArgsConstructor
+public class UiController implements Initializable {
+
+    private final JfxUiService jfxUiService;
+    private final TodoRepository todoRepository;
+
+    @FXML
+    public ListView<Todo> todosListView;
+    @FXML
+    public TextField todoTf;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        todosListView.setItems(FXCollections.observableArrayList(todoRepository.findAll()));
+    }
+
+    public void onExitMenuItemClick() {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void onAddTodoBtnClick() {
+        todoRepository.save(new Todo(null, todoTf.getText()));
+        updateTodosListView();
+    }
+
+    public void onDeleteAllTodosBtnClick() {
+        todoRepository.deleteAll();
+        updateTodosListView();
+    }
+
+    private void updateTodosListView() {
+        todosListView.setItems(FXCollections.observableArrayList(todoRepository.findAll()));
+    }
+
+    public void onOpenDialogMenuItemClick() throws IOException {
+        jfxUiService.createAndShowFxmlDialog(
+                "Non-Spring component dialog",
+                true,
+                true,
+                getClass().getResource("/com/wedasoft/SpaceEngineersModdingHelper/views/dialog.fxml"),
+                new Dimension2D(600, 400),
+                dialogController -> ((DialogController) dialogController).init());
+    }
+
+}
