7ddc28c4f87ae29bac20c3c50cdca32c4034ebe7
David Weber <weberdavid91@gmail.com>
2025-01-10 18:17:10 +0100

    delete unrelevant files and directories

--- src/main/java/com/wedasoft/SpaceEngineersModdingHelper/services/DeploymentService.java
+++ src/main/java/com/wedasoft/SpaceEngineersModdingHelper/services/DeploymentService.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Comparator;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -30,10 +32,11 @@ public class DeploymentService {
             throw new NotValidException("Your set path to appdata mods directory doesn't exist.");
         }
 
-        copyDirectory(modToDeploy.toPath(), new File(configurations.getPathToAppdataModsDirectory()).toPath());
+        Path copiedModPath = copyDirectory(modToDeploy.toPath(), new File(configurations.getPathToAppdataModsDirectory()).toPath());
+        deleteUnrelevantThings(copiedModPath);
     }
 
-    public void copyDirectory(Path dirToCopy, Path intoTargetParentDir) throws IOException {
+    private Path copyDirectory(Path dirToCopy, Path intoTargetParentDir) throws IOException {
         Files.walk(dirToCopy).forEach(sourcePath -> {
             try {
                 Path targetPath = intoTargetParentDir.resolve(dirToCopy.getFileName()).resolve(dirToCopy.relativize(sourcePath));
@@ -46,6 +49,32 @@ public class DeploymentService {
                 throw new RuntimeException("Error copying file: " + sourcePath, e);
             }
         });
+        return intoTargetParentDir.resolve(dirToCopy.getFileName());
     }
 
+    private void deleteUnrelevantThings(Path copiedModPath) throws IOException {
+        final Set<String> allowedNames = Set.of(
+                "thumb.png", "modinfo.sbmi", "metadata.mod",
+                "Audio", "Brains", "CustomWorlds", "Data", "DataPlatform", "Fonts", "InventoryScenes",
+                "Models", "Mods", "Particles", "Scenarios", "ShaderCache", "Shaders", "Textures", "Videos",
+                "VisualScripts", "VoxelMaps");
+
+        Files.walk(copiedModPath)
+                .sorted(Comparator.reverseOrder())
+                .forEach(p -> {
+                    try {
+                        if (!p.equals(copiedModPath)) { // don't delete the root director of the mod
+                            if (!allowedNames.contains(p.getFileName().toString())) {
+                                if (Files.isRegularFile(p)) {
+                                    Files.delete(p);
+                                } else if (Files.isDirectory(p)) {
+                                    Files.delete(p);
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
 }
