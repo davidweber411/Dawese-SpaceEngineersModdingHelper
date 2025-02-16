028f747557ac9c87866f4d5dc5141ceac3166788
David Weber <weberdavid91@gmail.com>
2025-02-16 18:56:04 +0100

    extracted internal dir paths into creationInfo

--- src/main/java/com/wedasoft/SpaceEngineersModdingHelper/services/createNewCharacterModService/CncmModelsDirSubService.java
+++ src/main/java/com/wedasoft/SpaceEngineersModdingHelper/services/createNewCharacterModService/CncmModelsDirSubService.java
@@ -1,21 +1,17 @@
 package com.wedasoft.SpaceEngineersModdingHelper.services.createNewCharacterModService;
 
-import com.wedasoft.SpaceEngineersModdingHelper.repositories.FileSystemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Files;
 
 @Service
 @RequiredArgsConstructor
 public class CncmModelsDirSubService {
 
-    private final FileSystemRepository fileSystemRepository;
-
     public void createInternalModelsSubDir(CncmCreationInfo creationInfo) throws IOException {
-        fileSystemRepository.createDirectoryIn(
-                creationInfo.getInternalKeyName(),
-                creationInfo.getModelsInternalKeyDir());
+        Files.createDirectories(creationInfo.getModelsInternalKeyDir());
     }
 
 }
--- src/main/java/com/wedasoft/SpaceEngineersModdingHelper/services/createNewCharacterModService/CncmTexturesDirSubService.java
+++ src/main/java/com/wedasoft/SpaceEngineersModdingHelper/services/createNewCharacterModService/CncmTexturesDirSubService.java
@@ -1,21 +1,17 @@
 package com.wedasoft.SpaceEngineersModdingHelper.services.createNewCharacterModService;
 
-import com.wedasoft.SpaceEngineersModdingHelper.repositories.FileSystemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Files;
 
 @Service
 @RequiredArgsConstructor
 public class CncmTexturesDirSubService {
 
-    private final FileSystemRepository fileSystemRepository;
-
     public void createInternalTexturesSubDir(CncmCreationInfo creationInfo) throws IOException {
-        fileSystemRepository.createDirectoryIn(
-                creationInfo.getInternalKeyName(),
-                creationInfo.getTexturesInternalKeyDir());
+        Files.createDirectories(creationInfo.getTexturesInternalKeyDir());
     }
 
 }
